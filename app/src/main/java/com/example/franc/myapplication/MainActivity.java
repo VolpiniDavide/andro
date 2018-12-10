b5357913abc0c6d27ac678128e543fdec7efe379
davide <volpini.davide1@gmail.com>
2018-12-10 15:54:01 +0100

    login and reg done

--- app/src/main/java/com/example/franc/myapplication/MainActivity.java
+++ app/src/main/java/com/example/franc/myapplication/MainActivity.java
@@ -1,17 +1,78 @@
 package com.example.franc.myapplication;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class MainActivity extends AppCompatActivity implements OnClickListener {
 
     private static final String TAG = "MainActivity";
+
+    EditText emailET;
+    EditText passwordET;
+
+    Button loginBtn;
+    Button registerBtn;
+
+
+    private boolean isValidEmail(){
+        return android.util.Patterns.EMAIL_ADDRESS.matcher(emailET.getText()).matches();
+    }
+
+    private boolean isValidPassword(){
+        String password = passwordET.getText().toString();
+        return (password.length() > 6);
+    }
+
+    private void showErrorMessage( String op){
+
+        Context context = getApplicationContext();
+        CharSequence text = op+" error!";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+
+    private void showSuccessMessage(String op){
+        Context context = getApplicationContext();
+        CharSequence text = op+" successful!";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        emailET = findViewById(R.id.email_et);         // col find view by id posso trovare un elemento del xml cui ho assegnato l'id specificato.
+        passwordET = findViewById(R.id.password_et);   // questo viene ritrvato nel file R sotto l'attributo "id". viee così tornato un qualsiasi
+        loginBtn = findViewById(R.id.login_btn);       // ogetto sottoclasse di view, n questo caso editText
+        registerBtn = findViewById(R.id.reg_btn);
+
+
+        registerBtn.setOnClickListener(this);
+        loginBtn.setOnClickListener(this);
+
+        emailET.getText();
+        passwordET.getText();
+
+
 
         Log.i(TAG, "activity created");
     }
@@ -45,4 +106,23 @@ public class MainActivity extends AppCompatActivity {
         super.onDestroy();
         Log.i(TAG, "activity destroyed");
     }
-}
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.reg_btn) {
+            if (isValidEmail()) {
+                if (isValidPassword())
+                    showSuccessMessage("login");
+                else showErrorMessage("invalid password");
+            } else if (!isValidEmail()) {
+                if (isValidPassword()) showErrorMessage("invalid email");
+                else showErrorMessage("invalid email and password");
+            }
+        } else if (view.getId() == R.id.login_btn) {
+            if (!isValidEmail() || !isValidPassword())
+                showErrorMessage("registration");
+            else showSuccessMessage("registration");
+        }
+    }
+
+    }
